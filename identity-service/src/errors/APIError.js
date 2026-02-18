5fe96f23c9ab0ddc95e28131907092525328fe1b
Ntokozokate <mangenakate@gmail.com>
2026-02-18 08:19:08 +0200

    Expanded my global error handler and refactored the controller logic

---
+++ identity-service/src/errors/APIError.js
@@ -0,0 +1,12 @@
+class APIError extends Error {
+  constructor(message, statusCode = 500) {
+    super(message);
+
+    this.statusCode = statusCode;
+    this.isOperational = true;
+
+    Error.captureStackTrace(this, this.constructor);
+  }
+}
+
+module.exports = APIError;
